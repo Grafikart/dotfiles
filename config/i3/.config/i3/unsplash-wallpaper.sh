9ffb04a1b2a77c82dc1e55aa68fb27e790fc8ad9
Grafikart <contact@grafikart.fr>
2017-09-28 16:23:07 +0200

    fix(unsplash wallpaper)

--- config/i3/.config/i3/unsplash-wallpaper.sh
+++ config/i3/.config/i3/unsplash-wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://source.unsplash.com/featured/1920x1080/daily/?landscape -O ~/Images/wallpaper.jpg
+wget "https://source.unsplash.com/featured/1920x1080/daily/?landscape" -O ~/Images/wallpaper.jpg
 feh --bg-scale ~/Images/wallpaper.jpg
