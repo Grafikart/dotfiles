6d78c7b81ae63738ff14057eca911ae84615e768
Jonathan <contact@grafikart.fr>
2018-04-12 00:28:34 +0200

    fix(bing wallpaper)

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -7,8 +7,8 @@ alias remove "pacaur -Rns"
 set -x EDITOR /usr/bin/vim
 set -x TERM xterm
 set -x ANDROID_HOME $HOME/.android
-set -x JAVA_HOME /usr/lib/jvm/java-8-jdk
-set -x PATH $PATH $JAVA_HOME/bin
+# set -x JAVA_HOME /usr/lib/jvm/java-8-jdk
+# set -x PATH $PATH $JAVA_HOME/bin
 
 # golang
 set -x GOPATH $HOME/.config/go
--- config/i3/.config/i3/bing-wallpaper.sh
+++ config/i3/.config/i3/bing-wallpaper.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+imagepath="$HOME/Images/bing-wallpaper.jpg"
 urlpath=$( \
 curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
 | xmllint --xpath "/rss/channel/item/link/text()" - \
 | sed 's/1366x768/1920x1080/g' \
 )
-curl "https://www.bing.com$urlpath" \
-| feh --bg-fill -
+curl "https://www.bing.com$urlpath" -o $imagepath
+feh --bg-fill $imagepath
