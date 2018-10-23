7b6553cefd949b9fb16f0e36e7bf850d6ee5ca34
Jonathan <contact@grafikart.fr>
2018-10-23 19:48:39 +0200

    update: nvidia

--- config/i3/.config/i3/bing-wallpaper.sh
+++ config/i3/.config/i3/bing-wallpaper.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 imagepath="$HOME/Images/bing-wallpaper.jpg"
 urlpath=$( \
+sleep 2
 curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
 | xmllint --xpath "/rss/channel/item/link/text()" - \
 | sed 's/1366x768/1920x1080/g' \
