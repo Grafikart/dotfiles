ed12172ebfe5930376a3cff57c858d05e57248cc
Jonathan <contact@grafikart.fr>
2025-01-31 10:29:03 +0100

    fix: windowrule for popups

--- config/hyprland/.config/hypr/startup.sh
+++ config/hyprland/.config/hypr/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 hyprctl dispatch exec [workspace 2 silent] kitty ~/Sites
-hyprctl dispatch workspace 3
-hyprctl dispatch exec [workspace 3] "flatpak run com.brave.Browser --profile-directory=\"Default\""
-sleep 1
-hyprctl dispatch workspace 7
-hyprctl dispatch exec [workspace 7] "flatpak run com.brave.Browser --profile-directory=\"Profile 1\""
+# hyprctl dispatch workspace 3
+# hyprctl dispatch exec [workspace 3] "flatpak run com.brave.Browser --profile-directory=\"Default\""
+# sleep 1
+# hyprctl dispatch workspace 7
+# hyprctl dispatch exec [workspace 7] "flatpak run com.brave.Browser --profile-directory=\"Profile 1\""
 #sleep 1
 #hyprctl dispatch workspace 1
 #hyprctl dispatch exec [workspace 1] phpstorm
--- config/hyprland/.config/hypr/toggle-waybar.sh
+++ config/hyprland/.config/hypr/toggle-waybar.sh
@@ -5,9 +5,13 @@ function restart_waybar
     if killall waybar
         hyprctl keyword general:gaps_out 0
         hyprctl keyword general:gaps_in 0
+        hyprctl keyword general:border_size 0
+        hyprctl keyword animations:enabled false
     else
         hyprctl keyword general:gaps_out 4
         hyprctl keyword general:gaps_in 4
+        hyprctl keyword general:border_size 2
+        hyprctl keyword animations:enabled true
         waybar &
     end
 end
